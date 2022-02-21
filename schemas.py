010a5b3675c7f410ca1b9a024dead6e03030967a
azzan-amin-97 <ibnuamin97@gmail.com>
2022-02-22 01:01:55 +0800

    update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,18 +1,8 @@
-from datetime import datetime
-from pydantic import BaseModel
+# use pydantic library
 
 
-class Booking(BaseModel):
-    name: str = "Aiman"
-    pax: int = 0
-    roomType: str = "Single Pod / Family Suite / Grand Deluxe"
-    checkIn: datetime = "2022-01-10 12:15:21"
-    checkOut: datetime = "2022-01-03 15:30:25"
-    paymentStatus: str  = "paid / unpaid"
+# create Booking Class
 
 
-class UpdateBooking(BaseModel):
-    pax: int = 0
-    roomType: str = "Single Pod / Family Suite / Grand Deluxe"
-    checkIn: datetime = "0000-00-00 00:00:00"
-    checkOut: datetime = "0000-00-00 00:00:00"
+# create UpdateBooking Class
+
